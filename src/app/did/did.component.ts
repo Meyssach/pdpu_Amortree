bdb839b2107f45993f9c512743fceeb17ea23e8e
Meyssa <meyssachab@gmail.com>
2018-08-21 07:42:09 +0200

    meyssa commit mardi

---
+++ src/app/did/did.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-did',
+  templateUrl: './did.component.html',
+  styleUrls: ['./did.component.scss']
+})
+export class DidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
